906ce2028214eabe14741eb83de22e7f94e14de3
hardbuilder <omrtakale@gmail.com>
2025-07-03 15:46:45 +0530

    Delete

--- my-portfolio/src/setupTests.js
+++
@@ -1,4 +0,0 @@
-// This file sets up the testing environment for the portfolio application.
-// You can add any necessary configuration or imports for your testing framework here.
-
-import '@testing-library/jest-dom/extend-expect';
